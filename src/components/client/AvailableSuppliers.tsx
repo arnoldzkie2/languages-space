d6d994887356ff5ad88ab9221e9786345196b2e5
Arnold <arnoldzkie22@gmail.com>
2023-12-05 16:39:36 +0800

    added something

--- src/components/client/AvailableSuppliers.tsx
+++ src/components/client/AvailableSuppliers.tsx
@@ -3,15 +3,17 @@
 import useClientStore, { bookingFormDataValue } from '@/lib/state/client/clientStore'
 import useAdminGlobalStore from '@/lib/state/super-admin/globalStore'
 import useAdminSupplierStore from '@/lib/state/super-admin/supplierStore'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const AvailableSuppliers = () => {
 
+    const router = useRouter()
     const { cards, getClientCards, availableSupplier, getAvailableSupplier, clearAvailableSuppliers, openBookingModal, bookingFormData, setBookingFormData } = useClientStore()
     const { setCardCourses } = useAdminSupplierStore()
     const [searchQuery, setSearchQery] = useState('')
@@ -38,7 +40,6 @@ const AvailableSuppliers = () => {
         }
     }
 
-
     useEffect(() => {
         setBookingFormData(bookingFormDataValue)
         clearAvailableSuppliers()
@@ -61,11 +62,23 @@ const AvailableSuppliers = () => {
         <div className='padding py-28 flex flex-col items-center text-slate-600'>
             <div className='w-full flex flex-col gap-5 sm:bg-white sm:p-5 sm:shadow 2xl:w-1/2'>
                 <div className='flex w-full items-center gap-5'>
-                    <select value={bookingFormData.clientCardID} name='clientCardID' onChange={(e) => setBookingFormData({ ...bookingFormData, clientCardID: e.target.value })} className='px-2 outline-none py-1.5 sm:py-2 text-sm bg-white border'>
+                    <select value={bookingFormData.clientCardID} name='clientCardID' onChange={(e) => {
+                        if (e.target.value === 'buy') {
+                            return router.push('/client/buy')
+                        }
+                        setBookingFormData({ ...bookingFormData, clientCardID: e.target.value })
+                    }
+                    }
+                        className='px-2 outline-none w-1/2 py-1.5 sm:py-2 text-sm bg-white border'>
                         <option value="" disabled>{ttt('client-card.select')}</option>
                         {cards && cards.length > 0 ? cards.map(card => (
                             <option value={card.id} key={card.id}>{card.name} ({card.balance})</option>
-                        )) : ''}
+                        )) : cards && cards.length === 0 ?
+                            <>
+                                <option disabled>{t('card.no-card')}</option>
+                                <option value='buy'>{t('card.buy')}</option>
+                            </>
+                            : <option disabled className='flex items-center gap-2'>{t('card.getting')}</option>}
                     </select>
                     <div className='w-full relative'>
                         <FontAwesomeIcon icon={faSearch} width={16} height={16} className='absolute right-2.5 sm:right-2 top-2.5 sm:top-3 text-slate-600' />
@@ -75,8 +88,8 @@ const AvailableSuppliers = () => {
                 <div className='w-full flex flex-wrap gap-8 justify-evenly'>
                     {availableSupplier && availableSupplier.length > 0 ? availableSupplier.map(supplierPrice => (
                         <div onClick={() => openBookingModal({ ...bookingFormData, supplierID: supplierPrice.supplier.id })} className='shadow rounded-md relative border hover:shadow-lg cursor-pointer w-full max-h-96 min-h-[384px] sm:w-80 overflow-y-auto bg-white items-center flex flex-col pb-3 p-5 gap-2' key={supplierPrice.supplier.id}>
-                            <Image width={125} height={125} className='w-[125px] min-w-[125px] min-h-[125px] bg-cover object-cover h-[125px] rounded-full' src={supplierPrice.supplier.profile_url || '/profile/profile.svg'} alt='Supplier Profile' />
-                            <h1 className='text-slate-700 h-8 px-5 text-center font-black uppercase'>{supplierPrice.supplier.name}</h1>
+                            <Image width={125} height={125} className='w-[125px] min-w-[125px] min-h-[125px] border max-w-[125px] max-h-[125px] object-cover h-[125px] rounded-full' src={supplierPrice.supplier.profile_url || '/profile/profile.svg'} alt='Supplier Profile' />
+                            <h1 className='text-slate-700 h-7 px-5 text-center font-black uppercase'>{supplierPrice.supplier.name}</h1>
                             <ul className='flex w-full flex-wrap gap-3 justify-center border-t pt-3'>
                                 {supplierPrice.supplier.tags.length > 0 && supplierPrice.supplier.tags.map((tag) => (
                                     <li key={tag} className='bg-slate-100 px-2 py-0.5 text-sm'>{tag}</li>
@@ -88,17 +101,26 @@ const AvailableSuppliers = () => {
                             </div>
                         </div>
                     ))
-                        : !bookingFormData.clientCardID ? <div className='py-20'>{t('booking.select-card')}</div> : bookingFormData.clientCardID && availableSupplier && !availableSupplier.length ? <div className='py-20'>{t('booking.no-supplier')}</div> : skeleton.map(item => (
-                            <div className='border shadow w-full bg-white sm:w-80 h-44 flex flex-col gap-2 p-5' key={item}>
-                                <h1 className='text-slate-700 border-b pb-2 w-44 h-8 bg-slate-200 animate-pulse rounded-md'></h1>
-                                <div className='h-5 w-40 bg-slate-200 animate-pulse rounded-md'></div>
-                                <div className='h-5 w-36 bg-slate-200 animate-pulse rounded-md'></div>
-                                <div className='w-full flex justify-between mt-auto gap-4'>
-                                    <div className='w-1/2 bg-slate-200 animate-pulse h-7 rounded-md'></div>
-                                    <div className='px-5 h-7 bg-blue-200 animate-pulse rounded-md w-1/3'></div>
+                        : !bookingFormData.clientCardID ? <div className='py-20'>{t('booking.select-card')}</div> : bookingFormData.clientCardID && availableSupplier && !availableSupplier.length
+                            ? <div className='py-20'>{t('booking.no-supplier')}</div>
+                            : skeleton.map(item => (
+                                <div className='shadow rounded-md relative border w-full max-h-96 min-h-[384px] sm:w-80 overflow-y-auto bg-white items-center flex flex-col pb-3 p-5 gap-2' key={item}>
+                                    <div className='border max-w-[125px] w-[125px] h-[125px] rounded-full bg-slate-100 animate-pulse'></div>
+                                    <h1 className='bg-slate-200 h-7 w-36 animate-pulse'></h1>
+                                    <ul className='flex w-full flex-wrap gap-3 justify-center border-t pt-3'>
+                                        <li className='bg-slate-100 w-16 animate-pulse h-5 text-sm'></li>
+                                        <li className='bg-slate-100 w-16 animate-pulse h-5 text-sm'></li>
+                                        <li className='bg-slate-100 w-16 animate-pulse h-5 text-sm'></li>
+                                        <li className='bg-slate-100 w-20 animate-pulse h-5 text-sm'></li>
+                                        <li className='bg-slate-100 w-20 animate-pulse h-5 text-sm'></li>
+                                        <li className='bg-slate-100 w-32 animate-pulse h-5 text-sm'></li>
+                                    </ul>
+                                    <div className='absolute right-0 bottom-0 w-full flex items-center h-16 px-5 border-t justify-between'>
+                                        <div className='h-5 w-20 bg-slate-200'></div>
+                                        <button className='bg-blue-300 animate-pulse w-28 rounded-sm h-7'></button>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
                 </div>
 
             </div>
